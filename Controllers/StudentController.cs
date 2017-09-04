7c7ddfaba8abac92040d22d61daaaba4164d98eb
clbokea <clbo@kea.dk>
2017-09-04 10:39:23 +0200

    view

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 namespace test.Controllers
 {
-    public class HomeController : Controller
+    public class StudentController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        public string day2()
+        {
+            return "Hello from day 2";
         }
     }
 }
