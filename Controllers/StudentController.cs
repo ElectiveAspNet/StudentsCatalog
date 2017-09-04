4d8b918a15839ad9bc21631d6a3baedb0fb8c891
clbokea <clbo@kea.dk>
2017-09-04 11:18:44 +0200

    mvc

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace test.Controllers
+namespace StudentsCatalog.Controllers
 {
     public class StudentController : Controller
     {
@@ -16,6 +16,12 @@ namespace test.Controllers
         public string day2()
         {
             return "Hello from day 2";
+        }
+
+        public IActionResult mvc(int id = 1000)
+        {
+            ViewBag.number = id;
+            return View();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace StudentsCatalog
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Student}/{action=Index}/{id?}");
             });
         }
     }
